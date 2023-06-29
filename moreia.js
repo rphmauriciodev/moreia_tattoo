8e73a568a736f08281fb9801c82cb9b529fa36c7
Condorruffles <65664248+Condorruffles@users.noreply.github.com>
2023-06-28 22:36:17 -0300

    a

--- moreia.js
+++ moreia.js
@@ -28,51 +28,6 @@ moreia.get('/', function(req,res){
 moreia.get('/sobreNos', function(req,res){
     res.render('sobreNos');
 })
-moreia.get('/login', function(req,res){
-    res.render('login');
-})
-moreia.get('/cadastro', function(req,res){
-    res.render('cadastro');
-})
-moreia.get('/dados', function(req,res){
-    res.render('dados',{lista});
-})
-moreia.post('/dados', function(req,res){
-    nome = req.body.nome
-    user = req.body.user
-    senha = req.body.senha
-    email = req.body.email
-    telefone = req.body.telefone
-
-    console.log(nome);
-    console.log(user);
-    console.log(senha);
-    console.log(email);
-    console.log(telefone);
-
-    pessoa = {
-        "nome":nome,
-        "user":user,
-        "senha":senha,
-        "email":email,
-        "telefone":telefone,
-    }
-    lista.push(pessoa);
-
-    res.render('dados',{lista})
-})
-
-moreia.post('/', function(req,res){
-    login = req.body.userLog;
-    senha = req.body.senhaLog;
-
-    if(login == "user" && senha == "1234"){
-        res.render('logado');
-    }else{
-        res.render('erro');
-    }
-})
-
 moreia.use(express.static('public'));
 
 moreia.listen(3000);
